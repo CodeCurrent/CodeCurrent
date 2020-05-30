f40db9d2d10f5586f4a311521c8cd16c14035f5e
Pratul Kumar <pratulkumar1997@gmail.com>
2020-05-31 05:15:15 +0530

    Chalk Basic Setup

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hello');
+const {error, success, warning } = require('./statics/chalk.color')
+
+console.log(error('This is a eroor'))
+console.log(success('this is good'));
+console.log(warning('Tihs is a warning'));
---
+++ statics/chalk.color.js
@@ -0,0 +1,18 @@
+chalk = require('chalk')
+
+const error = (text) => {
+    return chalk.bold.red(text)
+} 
+
+const success = (text) => {
+    return chalk.bold.greenBright(text)
+}
+const warning = (text) => {
+    return chalk.bold.yellow(text)
+}
+
+module.exports= {
+    error,
+    success,
+    warning 
+}
