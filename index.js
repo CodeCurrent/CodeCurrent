ee907d4cc82efdadbccf513f3f7295cd57a2bf02
Pratul Kumar <pratulkumar1997@gmail.com>
2020-06-05 17:30:24 +0530

    USER route GET and POST

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {error, success, warning } = require("./src/statics/chalk.color")
 const express = require("express")
+const bodyParser = require('body-parser')
 require("./src/db/mongoose")
 
 const USER = require("./src/models/user.model")
@@ -8,22 +9,48 @@ const QUESTION = require("./src/models/question.model")
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+app.use(bodyParser.json())
+
+app.get('/users', (req,res) => {
+    USER.find({}).then((users) => {
+        res.send(users)
+    }).catch((error) => {
+        res.send(error)
+    })
+})
+
+app.get('/users/:check_email', (req,res) => {
+    USER.findOne({
+        email : req.params.check_email
+    }).then((user) => {
+        if(!user){
+            return res.status(404).json({
+                status: 'error',
+                error: 'User does not Exist',
+              }).send()
+        }
+        res.send(user)
+    }).catch((error) => {
+        res.status(404).send(error)
+    })
+})
 
 app.post('/users', (req,res) => {
     const user = new USER(req.body)
-    user.save().then((user) =>{
+    user.save().then((user) => {
             res.status(201).send(user)
         }).catch((error) => {
             res.status(400).send(error)
         })
 })
 
+
+
 app.post('/questions', (req,res) => {
     const question = new QUESTION(req.body)
-    question.save().then((question) =>{
+    question.save().then((question) => {
         res.status(201).send(question)
-    }).catch((error) =>{
+    }).catch((error) => {
         res.status(400).send(error)
     })
 })
